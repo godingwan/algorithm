0818695ff31e9fc9e7a9bb7c52433c27d3a0b18c
Joshua Lee <jsungholee@gmail.com>
2015-04-06 21:27:33 -0400

    initial commit

---
+++ algorithm.cpp
@@ -0,0 +1,26 @@
+/*
+  Author: Joshua Lee
+  Date: 4/6/2015
+  Description: Program that will display a menu and allow the user to choose which 
+               search or sort algorithm to use until the user quits.
+  Algorithm:
+    1) Display a menu of the choices the user can choose
+    2) From the selection perform the search or sort that they chose
+    3) If it is a search function display the book in table format
+      a) take in the Book ID and the # of books bought
+      b) output the total cost
+    4) If it is a sorting function
+      a) display the unsorted array of 1,000 random numbers and then sort them
+      b) display the sorted array
+    5) Continue to loop through the menu until the user chooses to quit
+    6) Bonus: add a execution time calculation to both the searching and sorthing 
+              and display it to the user
+*/
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+  return 0;
+}
