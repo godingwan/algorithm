0d733c0bf0274e296d6c9052684f629e35d31507
Joshua Lee <jsungholee@gmail.com>
2015-04-06 21:41:15 -0400

    Main menu created

--- algorithm.cpp
+++ algorithm.cpp
@@ -21,6 +21,24 @@
 
 using namespace std;
 
+int displayMenu(){ // Function to display and return the user choice
+  int choice;
+  cout << "**** SEARCHING & SORTING MENU ****\n\n";
+  cout << "\t1. Linear Search\n";
+  cout << "\t2. Binary Search\n";
+  cout << "\t3. Bubble Sort\n";
+  cout << "\t4. Selection Sort\n";
+  cout << "\t5. Quit\n\n";
+  cout << "PLease enter your choice: ";
+  cin >> choice;
+  return choice;
+}
+
 int main(){
+  int choice = 0;
+  while (choice != 5){ // Continue to display the menu until they choose the quit option
+    choice = displayMenu();
+  }
+
   return 0;
 }
